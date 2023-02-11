1e2a34c1d9036909e47aa5a2d3d49742dbb18e44
ayush_0001 <ayushk0004@gmail.com>
2023-02-11 09:06:26 +0530

    Intro

---
+++ intro.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char name[10] = "AYUSH";
+    int regno = 12220741;
+    int rollno = 75;   
+    double contact = 8816912512;
+    float cgpa = 9.5;
+    printf("Your name is: %s\n",name);
+    printf("Your regno is: %d\n",regno);
+    printf("Your roll no is: %d\n",rollno);
+    printf("Your contact is: %lf\n",contact);
+    printf("Your cgpa is: %f\n",cgpa);
+    return 0;
+}
