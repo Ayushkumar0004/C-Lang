a82241342edb8683ca3d5646e9f79defb42330a4
ayush_0001 <ayushk0004@gmail.com>
2023-02-14 10:49:08 +0530

    Day-2

---
+++ Day2.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+// int main()
+// {
+//     int a = 1,b = 2,c;
+//     c = a > b;
+//     printf("\n%d",c);
+//     return 0;
+// }
+// wap to find wether the no is enter by user is smallest or greater out pf two variables
+// int main()
+// {
+//     int a,b;
+//     printf("Enter the a value: \n");
+//     scanf("%d",&a);
+//     printf("Enter the b value: \n");
+//     scanf("%d",&b);
+//     if (a > b)
+//     {
+//         printf("A is greater than B");
+//     }
+//     else
+//     {
+//         printf("A is less than B");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int a = 10, b = 0,c;
+//     c = a&&b;
+//     printf("%d",c);
+//     return 0;
+// }
+// int main()
+// {
+//     int a = 10, b = 0,c;
+//     c = a||b;
+//     printf("%d",c);
+//     return 0;
+// }
+// int main()
+// {
+//     int marks , attendance;
+//     printf("Enter the marks: \n");
+//     scanf("%d",&marks);
+//     printf("Enter the attendance: \n");
+//     scanf("%d",&attendance);
+//     if (marks > 76 && attendance >= 90)
+//     {
+//         printf("Keep it up!");
+//     }
+//     else{
+//         printf("You are suspended");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int a = 2 , b = 4;
+//     printf("a=%d,b=%d\n",a,b);
+//     printf("a&b=%d\n",a&b);
+//     printf("a|b%d\n",a|b);
+//     printf("a^b%d\n",a^b);
+//     printf("~a = %d\n",a = ~a);
+//     printf("b<<1 = %d\n",b<<1);
+//     printf("b>>1 = %d\n",b>>1);
+//     return 0;
+// }
+
+
+//CONDITIONAL OPERATORS
+// int main()
+// {
+//     int a , b;
+//     printf("Entter the value of a: \n");
+//     scanf("%d",&a);
+//     printf("Enter the value of b: \n");
+//     scanf("%d",&b);
+//     printf("a=%d,b=%d\n",a,b);
+//     printf("a&b=%d\n",a&b);
+//     printf("a|b%d\n",a|b);
+//     printf("a^b%d\n",a^b);
+//     printf("~a = %d\n",a = ~a); 
+//     printf("b<<1 = %d\n",b<<1);
+//     printf("b>>1 = %d\n",b>>1);
+//     return 0;
+// }
