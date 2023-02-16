26f8dcfc903f7b5d5131f06e5f5ce69344f72d5b
ayush_0001 <ayushk0004@gmail.com>
2023-02-16 12:26:15 +0530

    if-Elif

---
+++ if-else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,v;
+    printf("Enter the no: \n");
+    scanf("%d",&a);
+    printf("Enter the no v: \n");
+    scanf("%d",&v);
+    if (a<v)
+    {
+        printf("no is less than 10");
+    }
+    else
+    {
+        printf("no is greater than 100");
+    }    
+}
