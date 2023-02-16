1dc1ed505fa6494fdf175665788686be00faa1eb
ayush_0001 <ayushk0004@gmail.com>
2023-02-16 12:34:16 +0530

    task1

---
+++ task1.c
@@ -0,0 +1,17 @@
+// wap to find whethe the enter no is less than or greater than the other variable
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the first no: \n");
+    scanf("%d",&a);
+    printf("Enter the second no: \n");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("A is greater than B");
+    }
+    else{
+        printf("B is greater than A");
+    }
+}
