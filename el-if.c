bb634f1f36045fcb966d812d2b2a1aead8dd656c
ayush_0001 <ayushk0004@gmail.com>
2023-02-16 12:36:10 +0530

    El-If

---
+++ el-if.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+// int main()
+// {
+//     int v;
+//     printf("Enter the no: \n");
+//     scanf("%d", &v);
+//     if (v<10)
+//     {
+//         printf("no is less than 10");
+//     }
+//     else
+//     if(v<100)
+//     {
+//         printf("no is than 100");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     float marks;
+//     printf("Enter the marks: \n");
+//     scanf("%f",&marks);
+//     if (marks>90)
+//     {
+//         printf("Grade A");
+//     }
+//     else if(marks>80)
+//     {
+//         printf("Grade B");
+//     }
+//     else if(marks>70)
+//     {
+//         printf("Grade C");
+//     }
+//     else if(marks>60)
+//     {
+//         printf("Grade D");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     float marks;
+//     printf("Enter the marks: \n");
+//     scanf("%f",&marks);
+//     if (marks>90 && marks<=100)
+//     {
+//         printf("Grade A");
+//     }
+//     else if(marks>80 && marks<=90)
+//     {
+//         printf("Grade B");
+//     }
+//     else if(marks>70 && marks<=80)
+//     {
+//         printf("Grade C");
+//     }
+//     else if(marks>60 && marks<=70)
+//     {
+//         printf("Grade D");
+//     }
+//     else
+//     {
+//         printf("FAIL");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int i = 10;
+//     if(i==10)
+//     {
+//     if(i < 15)
+//     printf("I is samller than 15 \n");
+//     if(i < 12)
+//     printf("I is smaller than 12 too\n");
+//     else
+//     printf("I is greater than 15");
+//     }
+//     return 0;
+// }
