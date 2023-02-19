436923718fe9859844de6d256d76e92945453927
ayush_0001 <ayushk0004@gmail.com>
2023-02-19 17:58:51 +0530

    calculator

--- calculator.c
+++ calculator.c
@@ -2,29 +2,29 @@
 int main()
 {
     int n,a,b;
-    printf("enter first number=");
+    printf("enter first number: ");
     scanf("%d",&a);
-    printf("enter second number=");
+    printf("enter second number: ");
     scanf("%d",&b);
     printf("1.addition\n 2.subtraction\n 3.divison\n4.modulus\n5.multiplication\n");
-    printf("enter the choice=");
+    printf("enter the choice: ");
     scanf("%d",&n);
     switch(n)
     {
         case 1:
-            printf("addition=%d",a+b);
+            printf("addition: %d",a + b);
         break;
         case 2:
-            printf("subtraction=%d",a-b);
+            printf("subtraction: %d",a - b);
         break;
         case 3:
-            printf("divison=%d",a/b);
+            printf("divison: %d",a / b);
         break;
         case 4:
-            printf("modulus=%d",a%b);
+            printf("modulus: %d",a % b);
         break;
         case 5:
-            printf("multiplication=%d",a*b);
+            printf("multiplication: %d",a * b);
         break;
         default:
             printf("invalid input");
