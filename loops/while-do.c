b1c054d55a3757a04b2c60c13a3fb4a3fdc90e77
ayush_0001 <ayushk0004@gmail.com>
2023-02-21 10:00:37 +0530

    While-Do

---
+++ loops/while-do.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+// int main()
+// {
+//     int n = 10;
+//     do
+//     {
+//         printf("%d\n",n);
+//         n = n-1;
+//     }
+//     while(n>0);
+// }
+// int main()
+// {
+//     int n = 1;
+//     do
+//     {
+//         printf("%d\n",n);
+//         n = n+1;
+//     }
+//     while (n<11);
+// }
+// int main()
+// {
+//     int i,j;
+//     printf("Enter the no 1:\n");
+//     scanf("%d",&i);
+//     printf("Enter the no 2:\n");
+//     scanf("%d",&j);
+//     do
+//     {
+//         printf("\n%d * %d = %d" ,i ,j,(i*j));
+//         i++;
+//     }
+//     while(i<=10);
+// }
