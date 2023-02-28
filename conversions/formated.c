68680e15d60ad6cfee8819581d1e3bf84eb98ea0
ayush_0001 <ayushk0004@gmail.com>
2023-02-28 10:17:12 +0530

    format

---
+++ conversions/formated.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// int main()
+// {
+//     int a = 6543;
+//     printf("*%2d,*%-3d*",a,a);
+//     return 0;
+// }
+int main()
+{
+    int a = 123;
+    printf("*06%d*",a);
+    return 0;
+}
