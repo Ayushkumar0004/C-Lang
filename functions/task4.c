d81bf0cb1db16dcb583115b0be7e2424e405f140
ayush_0001 <ayushk0004@gmail.com>
2023-03-07 10:57:16 +0530

    math func

---
+++ functions/task4.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<math.h>
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The exponential is: %f",exp**(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The log value is: %f",log(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The log is: %f",log10(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The fabs value is: %f",fabs(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The ceil value is: %f",ceil(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The floor value is: %f",floor(x));
+// }
+
+// int main()
+// {
+//     int x,y;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("Enter the num: ");
+//     scanf("%d",&y);
+//     printf("The power value is: %f",pow(x,y));
+// }
+
+// int main()
+// {
+//     int x,y;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("Enter the num: ");
+//     scanf("%d",&y);
+//     printf("The fmod value is: %f",fmod(x,y));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The sin value is: %f",sin(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The cos value is: %f",cos(x));
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter the num: ");
+//     scanf("%d",&x);
+//     printf("The tan value is: %f",tan(x));
+// }
