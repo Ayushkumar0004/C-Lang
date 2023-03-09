86d0fe9f964c173444bbd1a06802b0b64c45158a
ayush_0001 <ayushk0004@gmail.com>
2023-03-09 12:06:54 +0530

    register-storage

---
+++ functions/register-class.c
@@ -0,0 +1,10 @@
+// #include<stdio.h>
+// int main()
+// {
+//     // register int i;i will be so frequently so,it can be given register dtorage class
+//     for(i=1; i<=20; i++)
+//     {
+//         printf("\n %d",i);
+//     }
+//     return 0;
+// }
