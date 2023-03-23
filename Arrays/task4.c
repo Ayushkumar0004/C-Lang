3638e40443bed885fb8320014fe222c4891682ef
ayush_0001 <ayushk0004@gmail.com>
2023-03-23 12:46:08 +0530

    1D-task4

--- Arrays/task4.c
+++ Arrays/task4.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    
+    int n,a[10],i,max,min;
+    printf("\nEnter number of elements: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min = a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]<min);
+        {
+            min=a[i];
+        }
+    max = a[0];
+    for(i=0; i<n;i++)
+    {
+        if(a[i]>max);
+        {
+            max = a[i];
+        }
+    }    
+    }
+    printf("\nMaximum element is: %d",max);
+    printf("\nMinimum element is: %d",min);
+    return 0;
 }
