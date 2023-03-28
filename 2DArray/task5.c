85c6dc2282e627fc122501b90c63b8c8465abba2
ayush_0001 <ayushk0004@gmail.com>
2023-03-28 10:38:37 +0530

    call by reference using Array

---
+++ 2DArray/task5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void reference(int[],int);
+int main()
+{
+    int arr[100],n;
+    int i;
+    printf("\nEnter n: ");
+    scanf("%d",&n);
+    printf("\nEnter array elements: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("\nElements by reference: ");
+    reference(arr,n);
+    return 0;
+}
+void reference(int x[],int size)
+{
+    int i;
+    for(i=0; i<size; i++)
+    {
+        printf("%d",x[i]);
+    }
+}
