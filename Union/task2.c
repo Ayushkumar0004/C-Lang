b7995e00c98ab9c50edc15c05abdf3c0ebed6a8b
ayush_0001 <ayushk0004@gmail.com>
2023-04-11 09:53:08 +0530

    display n number of record using union

---
+++ Union/task2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+union employee
+{
+    char name[20];
+    int id;
+    float salary;
+};
+int main()
+{
+    union employee u[100];
+    int n,i;
+    printf("\nEnter the value of n: ");
+    scanf("%d",&n);
+    fflush(stdin);
+    for(i=0; i<n; i++)
+    {
+        printf("\nEnter name: ");
+        fflush(stdin);
+        gets(u[i].name);
+        printf("\nEntered name is : %s",u[i].name);
+        printf("\nEnter id: ");
+        scanf("%d",&u[i].id);
+        printf("Enterd id is : %d",u[i].id);
+        printf("\nEnter Salary: ");
+        scanf("%d",&u[i].salary);
+        printf("Entered salary is : %2f",u[i].salary);
+    }
+    return 0;
+}
