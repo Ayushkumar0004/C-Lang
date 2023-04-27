74cace4518fbf89b2e49eff7fc7b427df8ad02fd
ayush_0001 <ayushk0004@gmail.com>
2023-04-27 12:20:18 +0530

    compare the string with static value

---
+++ String/task5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    char str1[20],str2[20];
+    int x;
+    printf("\nEnter the first string: ");
+    gets(str1);
+    printf("\nEnter the second string: ");
+    gets(str2);
+    x = strcpm(str1,str2);
+    if(x==0)
+    {
+        printf("\nString are equal");
+    }
+    else if(x>0)
+    {
+        printf("\nFirst string is greater than second string");
+    }
+    else
+    {
+        printf("\nFirst string is less than second string");
+    }
+    return 0;
+}
