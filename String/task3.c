e115641386da07be7655148135f766eed883b5bb
ayush_0001 <ayushk0004@gmail.com>
2023-04-27 12:00:22 +0530

    programme for cancatenation the string

---
+++ String/task3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[20],str2[10];
+    printf("\nEnter the first string: ");
+    gets(str1);
+    printf("\nEnter the second string: ");
+    gets(str2);
+    printf("\nString after cancatenation:%s",str1);
+    return 0;
+}
