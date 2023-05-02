07fbc34ae87461c97ed6cf572a1ca72beff59c91
ayush_0001 <ayushk0004@gmail.com>
2023-05-02 09:50:58 +0530

    reverse of string using without inbuilt function

---
+++ String/task15.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[100],str2[100],str3[200],temp;
+    int i=0,j=0;
+    printf("\nEnter the first string: ");
+    gets(str1);
+    printf("\nEnter the first string: ");
+    gets(str2);
+    while(str1[i]!='\0')
+    {
+        str3[j] = str1[i];
+        i++;
+        j++;
+    }
+    i=0;
+    while(str2[i]!='\0')
+    {
+        str3[j] = str2[i];
+        i++;
+        j++;
+    }
+    str3[j] = '\0';
+    printf("The cancatenated string is: ");
+    puts(str3);
+    i = 0;;
+    j = strlen(str3)-1;
+    while(i<j)
+    {
+        temp = str3[i];
+        str3[i] = str3[j];
+        str3[j] = temp;
+        i++;
+        j--;
+    }
+    printf("Reverse string is: %s",str3);
+    return 0;
+}
